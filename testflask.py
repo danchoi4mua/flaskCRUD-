47c76d87c2cc20dd88b1fb64f7c7913686537c1c
danchoi4mua <89177451+danchoi4mua@users.noreply.github.com>
2022-09-03 22:22:43 +0700

    Add files via upload

--- testflask.py
+++ testflask.py
@@ -1,14 +1,18 @@
 from flask import Flask , render_template , request ,redirect ,url_for ,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 from flask_wtf.csrf import CSRFProtect
 from flask.helpers import flash
-
+from flask_login import LoginManager ,UserMixin ,login_user , logout_user ,current_user
+from enum import Enum as userEnum
+from flask_admin import BaseView, expose
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+mysqldb://root:123123@127.0.0.1/sinhvien'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+login = LoginManager(app)
 
 app.config['SECRET_KEY'] = '12345678'
 class sinhvien(db.Model):
@@ -16,17 +20,90 @@ class sinhvien(db.Model):
     ten = db.Column(db.String(100))
     masosv = db.Column(db.Integer)
     email = db.Column(db.String(100))
-    def __init__(self,ten,masosv,email):
-        self.ten = ten
-        self.masosv = masosv
-        self.email = email
 
 
-#@app.before_first_request
-#def create_table():
-#    db.create_all()
 
-@app.route("/")
+class quyenUser(userEnum):
+    admin=1
+    user=2
+
+class taikhoan2(db.Model,UserMixin):
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    taikhoan = db.Column(db.String(100))
+    matkhau = db.Column(db.String(100))
+    quyen = db.Column(Enum(quyenUser), default = quyenUser.user)
+
+    def __init__(self,taikhoan,matkhau,quyen):
+        self.taikhoan = taikhoan
+        self.matkhau = matkhau
+        self.quyen = quyen
+    def __repr__(self) -> str:
+        return f'<User {self.taikhoan}>'
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+
+@login.user_loader
+def load_user(user_id):
+    return taikhoan2.query.get(user_id)
+
+@app.route("/dangxuat")
+def dangxuat123():
+    logout_user()
+    return redirect("/")
+
+
+@app.route("/dangki" , methods = ['GET','POST'])
+def dangki():
+
+    if request.method == 'GET':
+        return render_template("dangki.html")
+    if request.method == 'POST':
+        taikhoan = request.form['tendangnhap']
+        matkhau = request.form['matkhautaikhoan']
+        quyen = request.form['quyen']
+        themvodb = taikhoan2(taikhoan=taikhoan, matkhau=matkhau,quyen=quyen)
+        db.session.add(themvodb)
+        db.session.commit()
+        return redirect("/")
+
+@app.route("/" , methods = ['GET','POST'])
+def dangnhap():
+
+    return render_template("dangnhap.html")
+
+
+#
+
+
+
+
+
+@app.route("/xacminhdangnhap" , methods = ['GET','POST'])
+def xacminhdangnhap():
+    if request.method == 'POST':
+        ten = request.form['tendangnhap']
+        matkhau = request.form['matkhautaikhoan']
+        xacminh = taikhoan2.query.filter(taikhoan2.taikhoan == ten,
+                                        taikhoan2.matkhau == matkhau).first()
+        if xacminh :
+            login_user(user=xacminh)
+            if current_user.is_authenticated and current_user.quyen == quyenUser.admin :
+                quyentk = 1
+            else:
+                quyentk = 2
+            return render_template("dangnhap.html" , quyentk = quyentk)
+        else :
+            return redirect("/create")
+
+
+@app.route("/read")
 def hello_world():
     data = sinhvien.query.all()
     return render_template("read.html",data2 = data)
@@ -43,7 +120,8 @@ def create():
         themvodb = sinhvien(ten=ten, email=email, masosv=masosv)
         db.session.add(themvodb)
         db.session.commit()
-        return redirect("/")
+        return redirect("/read")
+
 
 
 
@@ -92,7 +170,7 @@ def thaydoi():
     #data.email = emailsauthaydoi
     #db.session.commit()
     #return jsonify({'thanhcong': True},)
-    return 
+    return
 
 
 
